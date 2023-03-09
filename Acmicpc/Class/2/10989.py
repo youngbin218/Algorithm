1d10c21e8b0c4081c6b89e8ff99d3ecfb8e0326b
Youngbin Ki <50580794+youngbin218@users.noreply.github.com>
2023-03-09 16:25:49 +0900

    10989번: 수 정렬하기 3
    https://www.acmicpc.net/problem/10989

---
+++ Acmicpc/Class/2/10989.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0] * 10000
+
+for _ in range(N):
+    num = int(input())
+    arr[num-1] += 1
+
+for i in range(10000):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i+1)
